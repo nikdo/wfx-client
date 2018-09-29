6a506b5b501733d3883c815fdfd0e6b6ebd60182
Radek Matěj <radek@nikdo.cz>
2018-09-29 22:57:41 +0200

    Show country and region in spot selector

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default ({ options, onChange }) => (
   <select onChange={onChange}>
-    {options.map((option) =>
-      <option value={option._id} key={option._id}>{option.name}</option>
+    {options.map(spot =>
+      <option value={spot._id} key={spot._id}>
+        {spot.country + ' '}
+        {spot.region && spot.region + ': '}
+        {spot.name}
+      </option>
     )}
   </select>
 )
