95c89723996873071a508e69acb9b216319e8835
Radek Matěj <radek@nikdo.cz>
2018-10-06 15:10:39 +0200

    Specify hover tooltip height in px

--- src/components/transformations/transform.js
+++ src/components/transformations/transform.js
@@ -4,10 +4,10 @@ import tickValues from './tickValues'
 const bftCeilings = [0, 1.5, 3.3, 5.5, 8, 10.8, 13.9, 17.2, 20.7, 24.5, 28.4, 32.6]
 const minEndValue = 16
 const unitHeigth = 12
-const hoverTooltipHeight = 2
+const hoverTooltipHeight = 30
 
 export default data => {
-  const yMaxValue = max(data.map(d => d.windSpeed)) + hoverTooltipHeight
+  const yMaxValue = max(data.map(d => d.windSpeed)) + hoverTooltipHeight / unitHeigth
   const windTickValues = tickValues(bftCeilings, Math.max(yMaxValue, minEndValue))
   const yEndValue = windTickValues[windTickValues.length - 1]
 
