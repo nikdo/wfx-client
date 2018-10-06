3065c2de047fb0b50af05e678eae6d0b39ab7ad3
Radek Matěj <radek@nikdo.cz>
2018-10-06 13:27:17 +0200

    Extract transformations

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,39 +1,7 @@
 import React, { Component } from 'react'
-import { scalePoint, scaleLinear, max } from 'd3'
 import './Chart.css'
+import transform from './transformations/transform'
 import windChart from '../charts/windChart'
-
-const ceilToEven = num => 2 * Math.ceil(num / 2)
-
-const evenNumbers = max => [...Array(max).keys()]
-  .map(i => ++i)
-  .filter(i => !(i % 2))
-
-const getVisualisations = data => {
-  const hoverTooltipHeight = 2
-  const minEndValue = 16
-  const yMax = ceilToEven(max([
-    ...data.map(d => d.windSpeed + hoverTooltipHeight),
-    minEndValue
-  ]))
-
-  const unitHeigth = 12
-  const dimensions = {
-    w: 1200,
-    h: yMax * unitHeigth
-  }
-  const scales = {
-    x: scalePoint()
-      .domain(data.map(d => d.time))
-      .range([0, dimensions.w]),
-    y: scaleLinear()
-      .domain([0, yMax])
-      .rangeRound([dimensions.h, 0])
-  }
-  const windTickValues = evenNumbers(yMax)
-
-  return { dimensions, scales, windTickValues }
-}
 
 export default class Chart extends Component {
   constructor (props) {
@@ -54,7 +22,7 @@ export default class Chart extends Component {
   }
 
   static getDerivedStateFromProps (props) {
-    return { visualisations: getVisualisations(props.forecast) }
+    return { visualisations: transform(props.forecast) }
   }
 
   componentDidMount () {
---
+++ src/components/transformations/transform.js
@@ -0,0 +1,33 @@
+import { scalePoint, scaleLinear, max } from 'd3'
+
+const ceilToEven = num => 2 * Math.ceil(num / 2)
+
+const evenNumbers = max => [...Array(max).keys()]
+  .map(i => ++i)
+  .filter(i => !(i % 2))
+
+export default data => {
+  const hoverTooltipHeight = 2
+  const minEndValue = 16
+  const yMax = ceilToEven(max([
+    ...data.map(d => d.windSpeed + hoverTooltipHeight),
+    minEndValue
+  ]))
+
+  const unitHeigth = 12
+  const dimensions = {
+    w: 1200,
+    h: yMax * unitHeigth
+  }
+  const scales = {
+    x: scalePoint()
+      .domain(data.map(d => d.time))
+      .range([0, dimensions.w]),
+    y: scaleLinear()
+      .domain([0, yMax])
+      .rangeRound([dimensions.h, 0])
+  }
+  const windTickValues = evenNumbers(yMax)
+
+  return { dimensions, scales, windTickValues }
+}
