d05615c8fac77f564571fbdb619a8fb8e5f6979b
Radek Matěj <radek@nikdo.cz>
2018-10-20 14:55:53 +0200

    Extract component for hover value tooltip

--- src/charts/hoverGuide.js
+++ src/charts/hoverGuide.js
@@ -1,23 +1,5 @@
-import { bisect } from 'd3'
 import { lineHeight } from './constants'
-
-const bftNames = [
-  'Calm',
-  'Light Air',
-  'Light Breeze',
-  'Gentle Breeze',
-  'Moderate Breeze',
-  'Fresh Breeze',
-  'Strong Breeze',
-  'Near Gale',
-  'Gale',
-  'Strong Gale',
-  'Storm',
-  'Violent Storm',
-  'Hurricane Force'
-]
-
-const toBft = (value, bftCeilings) => bisect(bftCeilings, value)
+import hoverValueTooltip from './hoverValueTooltip'
 
 export default (canvas, dimensions, scales, data, bftCeilings, subscribeToHoverEvents) => {
   const hoverGuide = canvas.append('g')
@@ -33,28 +15,7 @@ export default (canvas, dimensions, scales, data, bftCeilings, subscribeToHoverE
   const value = hoverGuide.append('g')
     .attr('class', 'value')
 
-  value.append('circle')
-    .attr('r', 3)
-
-  value.append('text')
-    .attr('class', 'ms')
-    .attr('dy', lineHeight / 4)
-
-  value.append('text')
-    .attr('class', 'bft')
-    .attr('dy', lineHeight * 1.5)
-
-  value.selectAll('text')
-    .attr('x', lineHeight / 2)
-    .attr('paint-order', 'stroke')
-    .style('paint-order', 'stroke') // Safari needs to set style when stroke is set in CSS
-    .attr('stroke-linejoin', 'round')
-
-  value.append('g')
-    .attr('class', 'wind-direction')
-    .attr('transform', `translate(${lineHeight}, ${-lineHeight * 1.5})`)
-    .append('path')
-    .attr('d', 'M-2.5,1 L0,5 L2.5,1 M0,4 V -10')
+  hoverValueTooltip(value, data, bftCeilings, subscribeToHoverEvents)
 
   const time = hoverGuide.append('g')
     .attr('class', 'time')
@@ -72,24 +33,12 @@ export default (canvas, dimensions, scales, data, bftCeilings, subscribeToHoverE
   subscribeToHoverEvents({
     onMouseOut: () => hoverGuide.style('display', 'none'),
     onValueHover: (x, i) => {
-      const windSpeed = data[i].windSpeed
-      const windGustDiff = data[i].windGust - windSpeed
-      const y = scales.y(windSpeed)
-      const bft = toBft(windSpeed, bftCeilings)
+      const y = scales.y(data[i].windSpeed)
       hoverGuide
         .style('display', null)
         .attr('transform', `translate(${x}, 0)`)
       line.attr('y1', y)
       value.attr('transform', `translate(0, ${y})`)
-      value.select('text.ms')
-        .text(windSpeed.toFixed(1) + '\u2009m/s')
-        .append('tspan').attr('class', 'gusts').text(' +' + windGustDiff.toFixed(1))
-      value.select('text.bft')
-        .text(bft + '\u2009bft')
-        .attr('class', `bft level-${bft}`)
-        .attr('display', bft > 1 ? null : 'none')
-        .append('tspan').text(' ' + bftNames[bft])
-      value.select('.wind-direction path').attr('transform', `rotate(${data[i].windBearing})`)
       time.select('.time text').text(scales.x.domain()[i].format('HH:mm'))
     }
   })
---
+++ src/charts/hoverValueTooltip.js
@@ -0,0 +1,62 @@
+import { bisect } from 'd3'
+import { lineHeight } from './constants'
+
+const bftNames = [
+  'Calm',
+  'Light Air',
+  'Light Breeze',
+  'Gentle Breeze',
+  'Moderate Breeze',
+  'Fresh Breeze',
+  'Strong Breeze',
+  'Near Gale',
+  'Gale',
+  'Strong Gale',
+  'Storm',
+  'Violent Storm',
+  'Hurricane Force'
+]
+
+const toBft = (value, bftCeilings) => bisect(bftCeilings, value)
+
+export default (value, data, bftCeilings, subscribeToHoverEvents) => {
+  value.append('circle')
+    .attr('r', 3)
+
+  const ms = value.append('text')
+    .attr('class', 'ms')
+    .attr('dy', lineHeight / 4)
+
+  const bf = value.append('text')
+    .attr('class', 'bft')
+    .attr('dy', lineHeight * 1.5)
+
+  value.selectAll('text')
+    .attr('x', lineHeight / 2)
+    .attr('paint-order', 'stroke')
+    .style('paint-order', 'stroke') // Safari needs to set style when stroke is set in CSS
+    .attr('stroke-linejoin', 'round')
+
+  const direction = value.append('g')
+    .attr('class', 'wind-direction')
+    .attr('transform', `translate(${lineHeight}, ${-lineHeight * 1.5})`)
+    .append('path')
+    .attr('d', 'M-2.5,1 L0,5 L2.5,1 M0,4 V -10')
+
+  subscribeToHoverEvents({
+    onValueHover: (x, i) => {
+      const windSpeed = data[i].windSpeed
+      const windGustDiff = data[i].windGust - windSpeed
+      const bft = toBft(windSpeed, bftCeilings)
+      ms.text(windSpeed.toFixed(1) + '\u2009m/s')
+        .append('tspan')
+        .attr('class', 'gusts')
+        .text(' +' + windGustDiff.toFixed(1))
+      bf.text(bft + '\u2009bft')
+        .attr('class', `bft level-${bft}`)
+        .attr('display', bft > 1 ? null : 'none')
+        .append('tspan').text(' ' + bftNames[bft])
+      direction.attr('transform', `rotate(${data[i].windBearing})`)
+    }
+  })
+}
