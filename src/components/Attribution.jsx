fb934e670e6d945137e0df7acb72db70bf99095e
Radek Matěj <radek@nikdo.cz>
2019-02-01 18:40:52 +0100

    Add Dark Sky logo

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { ReactComponent as DarkSkyLogo } from '../img/dark-sky.svg'
 import './Attribution.css'
 
 export default () => (
-  <a href='https://darksky.net/poweredby/'>Powered by Dark Sky</a>
+  <a href='https://darksky.net/poweredby/'>
+    <DarkSkyLogo height='17' width='13' />&nbsp;<span>Powered by Dark Sky</span>
+  </a>
 )
