b555348802a81e1a052f2320c65266af07e5d32e
Radek Matěj <radek@nikdo.cz>
2019-02-02 21:17:13 +0100

    Change attribution styles to be component-scoped

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ReactComponent as DarkSkyLogo } from '../img/dark-sky.svg'
-import './Attribution.css'
+import styles from './Attribution.module.css'
 
 export default () => (
-  <a href='https://darksky.net/poweredby/'>
+  <a className={styles.attribution} href='https://darksky.net/poweredby/'>
     <DarkSkyLogo height='17' width='13' />&nbsp;<span>Powered by Dark Sky</span>
   </a>
 )
