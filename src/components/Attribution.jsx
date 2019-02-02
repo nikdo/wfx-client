9324361c0b8bcd8c129d7774bf953c4202d2f60f
Radek Matěj <radek@nikdo.cz>
2019-02-02 21:25:33 +0100

    Fix chart to attribution spacing

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -3,7 +3,9 @@ import { ReactComponent as DarkSkyLogo } from '../img/dark-sky.svg'
 import styles from './Attribution.module.css'
 
 export default () => (
-  <a className={styles.attribution} href='https://darksky.net/poweredby/'>
-    <DarkSkyLogo height='17' width='13' />&nbsp;<span>Powered by Dark Sky</span>
-  </a>
+  <p className={styles.attribution}>
+    <a href='https://darksky.net/poweredby/'>
+      <DarkSkyLogo height='17' width='13' />&nbsp;<span>Powered by Dark Sky</span>
+    </a>
+  </p>
 )
