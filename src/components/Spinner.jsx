cb963dc8760d7884f048c2857de96545c83e01a2
Radek Matěj <radek@nikdo.cz>
2019-02-17 11:07:08 +0100

    Change spinner style to be component-scoped

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './Spinner.css'
+import styles from './Spinner.module.css'
 
 // https://codepen.io/supah/pen/BjYLdW
-export default () => <svg className='spinner' viewBox='0 0 50 50'>
-  <circle className='path' cx='25' cy='25' r='20' fill='none' strokeWidth='5' />
+export default () => <svg className={styles.spinner} viewBox='0 0 50 50'>
+  <circle className={styles.path} cx='25' cy='25' r='20' fill='none' strokeWidth='5' />
 </svg>
