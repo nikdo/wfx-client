6dd038f5e4b1569c3c67cc65a6eff0809e5dddbb
Radek Matěj <radek@nikdo.cz>
2019-02-17 16:21:02 +0100

    Move spinner positioning to inline styles

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import styles from './Spinner.module.css'
 
 // https://codepen.io/supah/pen/BjYLdW
-export default () => <svg className={styles.spinner} viewBox='0 0 50 50'>
-  <circle className={styles.path} cx='25' cy='25' r='20' fill='none' strokeWidth='5' />
-</svg>
+export default () => {
+  const dimension = 50
+  const position = {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    width: dimension,
+    height: dimension,
+    marginLeft: -0.5 * dimension,
+    marginTop: -0.5 * dimension
+  }
+  const strokeWidth = 4
+  return <svg className={styles.spinner} viewBox='0 0 50 50' style={position}>
+    <circle className={styles.path} cx='25' cy='25' r='20' strokeWidth={strokeWidth} />
+  </svg>
+}
