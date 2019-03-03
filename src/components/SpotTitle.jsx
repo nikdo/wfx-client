d888b9ef3d1b1ff0fdb68d8df96ceca910ad2b59
Radek Matěj <radek@nikdo.cz>
2019-03-03 14:57:56 +0100

    Right-align search on desktop and landscape devices

--- src/components/SpotTitle.jsx
+++ src/components/SpotTitle.jsx
@@ -3,6 +3,6 @@ import location from './location'
 import styles from './SpotTitle.module.css'
 
 export default ({ spot }) => <header>
-  <h1>{spot.name}</h1>
+  <h1 className={styles.title}>{spot.name}</h1>
   <p className={styles.location}>{location(spot)}</p>
 </header>
