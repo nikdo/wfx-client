5ab2e6f4f1656b4ef5dc7481019a1d2c717af61d
Radek Matěj <radek@nikdo.cz>
2019-03-03 15:54:23 +0100

    Move Spinner to Selector component

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Select from 'react-select'
 import location from './location'
+import Spinner from './Spinner'
 import styles from './Selector.module.css'
 
 const options = spots => spots.map(spot => ({
@@ -8,16 +9,23 @@ const options = spots => spots.map(spot => ({
   value: spot._id
 }))
 
-export default ({ value, disabled, spots, onChange }) => (
-  <Select
-    value={options(spots).find(option => option.value === value)}
-    onChange={option => onChange(option.value)}
-    options={options(spots)}
-    isDisabled={disabled}
-    blurInputOnSelect
-    isClearable={false}
-    backspaceRemovesValue={false}
-    className={styles.select}
-    classNamePrefix='select'
-  />
+export default ({ value, spotLoading, spots, onChange }) => (
+  <>
+    <Select
+      value={options(spots).find(option => option.value === value)}
+      onChange={option => onChange(option.value)}
+      options={options(spots)}
+      isDisabled={spotLoading}
+      blurInputOnSelect
+      isClearable={false}
+      backspaceRemovesValue={false}
+      className={styles.select}
+      classNamePrefix='select'
+    />
+    {spotLoading &&
+      <div className={styles.loading}>
+        <Spinner inline />
+      </div>
+    }
+  </>
 )
