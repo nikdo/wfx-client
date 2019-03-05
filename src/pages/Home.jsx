49156eb821c48d0522cce17edc64ded7ffb7ba69
Radek Matěj <radek@nikdo.cz>
2019-03-05 08:53:32 +0100

    Add search input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default ({ spots, onSpotSelected }) => <ul>
-  {spots.map(spot =>
-    <li onClick={() => onSpotSelected(spot._id)}>
-      {spot.name}
-    </li>
-  )}
-</ul>
+export default class Home extends Component {
+  constructor () {
+    super()
+    this.state = { query: '' }
+  }
+
+  handleChange = event => {
+    this.setState({ query: event.target.value })
+  }
+
+  render () {
+    return <>
+      <input type='text' value={this.state.query} onChange={this.handleChange} />
+      <ul>
+        {this.props.spots.map(spot =>
+          <li onClick={() => this.props.onSpotSelected(spot._id)}>
+            {spot.name}
+          </li>
+        )}
+      </ul>
+    </>
+  }
+}
