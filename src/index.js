1e9e9699aaf2282a4fb19707d3d74d46d3cc8b74
Radek Matěj <radek@nikdo.cz>
2019-03-11 12:32:58 +0100

    Use redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={createStore(rootReducer, composeWithDevTools())}>
     <App />
   </Provider>,
   document.getElementById('root')
