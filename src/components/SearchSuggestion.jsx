a570ef32f2a2c5bbdbdc3f89a45199ae6b1ff099
Radek Matěj <radek@nikdo.cz>
2019-03-15 18:01:24 +0100

    Extract highlighted text markup

--- src/components/SearchSuggestion.jsx
+++ src/components/SearchSuggestion.jsx
@@ -3,21 +3,23 @@ import classNames from 'classnames'
 import parse from 'autosuggest-highlight/parse'
 import styles from './SearchSuggestion.module.css'
 
+const HighlightedText = ({ text, matches }) => <>
+  {parse(text, matches).map((fragment, i) =>
+    <span key={i} className={classNames({ [styles.highlight]: fragment.highlight })}>
+      {fragment.text}
+    </span>
+  )}
+</>
+
 export default ({ spot }) => <>
   <div>
-    {parse(spot.name, spot.nameMatches).map((fragment, i) =>
-      <span key={i} className={classNames({ [styles.highlight]: fragment.highlight })}>
-        {fragment.text}
-      </span>
-    )}
+    <HighlightedText text={spot.name} matches={spot.nameMatches} />
   </div>
   <div className={styles.location}>
-    {spot.region && parse(spot.region, spot.regionMatches).map((fragment, i) =>
-      <span key={i} className={classNames({ [styles.highlight]: fragment.highlight })}>
-        {fragment.text}
-      </span>
-    )}
-    {spot.region && ', '}
+    {spot.region && <>
+      <HighlightedText text={spot.region} matches={spot.regionMatches} />
+      {', '}
+    </>}
     <span>{spot.country}</span>
   </div>
 </>
