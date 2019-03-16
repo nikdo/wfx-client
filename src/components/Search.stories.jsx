e70929c19822146b7f2bdb76f47e19511fcbc8c8
Radek Matěj <radek@nikdo.cz>
2019-03-16 15:27:29 +0100

    Add more search stories

--- src/components/Search.stories.jsx
+++ src/components/Search.stories.jsx
@@ -5,5 +5,15 @@ import Search from './Search'
 const WhitespaceDecorator = storyFn => <div style={{ margin: 24 }}>{storyFn()}</div>
 addDecorator(WhitespaceDecorator)
 
+const defaultProps = {
+  spots: [],
+  query: 'la'
+}
+
 storiesOf('Search', module)
-  .add('empty', () => <Search fat spots={[]} query='' />)
+  .add('empty', () => <Search {...defaultProps} query='' />)
+  .add('query', () => <Search {...defaultProps} autoFocus />)
+  .add('loading', () => <Search {...defaultProps} spotLoading />)
+  .add('fat empty', () => <Search {...defaultProps} fat query='' />)
+  .add('fat query', () => <Search {...defaultProps} fat autoFocus />)
+  .add('fat loading', () => <Search {...defaultProps} fat spotLoading />)
