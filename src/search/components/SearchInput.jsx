e22c0e78c6bcfde22c5cfb15d359c2a1a96275a9
Radek Matěj <radek@nikdo.cz>
2019-03-16 17:34:52 +0100

    Add search icon

--- src/search/components/SearchInput.jsx
+++ src/search/components/SearchInput.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { ReactComponent as SearchIcon } from './search.svg'
 import Spinner from '../../components/Spinner'
 import styles from './SearchInput.module.css'
 
 export default ({ spotLoading, inputProps }) => (
   <div className={styles.inputContainer}>
     <input {...inputProps} />
-    {spotLoading &&
+    {spotLoading ? (
       <Spinner inline />
-    }
+    ) : (
+      <SearchIcon className={styles.searchIcon} />
+    )}
   </div>
 )
