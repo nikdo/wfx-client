84f2863fa8211e350639ea16898a0614913ddad3
Radek Matěj <radek@nikdo.cz>
2019-03-17 23:13:27 +0100

    Fix transition duration

--- src/components/OpacityTransition.jsx
+++ src/components/OpacityTransition.jsx
@@ -3,7 +3,7 @@ import { CSSTransition } from 'react-transition-group'
 import styles from './OpacityTransition.module.css'
 
 export default (props) => (
-  <CSSTransition in={props.in} classNames={styles} timeout={300} unmountOnExit>
+  <CSSTransition in={props.in} classNames={styles} timeout={200} unmountOnExit>
     {props.children}
   </CSSTransition>
 )
