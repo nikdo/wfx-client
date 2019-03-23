44146d64d7c1430d1e703f2bdcef6c9cc6d0c854
Radek Matěj <radek@nikdo.cz>
2019-03-23 17:43:37 +0100

    And logo text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import styles from './Home.module.css'
 
 export default () => {
   return <header className={styles.header}>
+    <h1>Wind Freaks</h1>
     <SearchContainer autoFocus fat />
   </header>
 }
