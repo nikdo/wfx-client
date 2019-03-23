e048c2db64907a88363994045accc185081bb964
Radek Matěj <radek@nikdo.cz>
2019-03-23 20:44:12 +0100

    Fix fat search storybook

--- src/search/components/Search.stories.jsx
+++ src/search/components/Search.stories.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import { storiesOf, addDecorator } from '@storybook/react'
+import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Search from './Search'
 
-const WhitespaceDecorator = storyFn => <div style={{ margin: 24 }}>{storyFn()}</div>
-addDecorator(WhitespaceDecorator)
+const WhitespaceDecorator = (gradient = false) => storyFn => {
+  const style = {
+    padding: 24,
+    background: gradient
+      ? `linear-gradient(
+        120deg,
+        hsla(239, 35%, 51%, 0.83),
+        hsla(219, 44%, 50%, 0.83) 70%
+      )`
+      : 'transparent'
+  }
+  return <div style={style}>{storyFn()}</div>
+}
+
+const TransparentDecorator = WhitespaceDecorator()
+const GradientDecorator = WhitespaceDecorator(true)
 
 const defaultProps = {
   spots: [],
@@ -13,9 +27,13 @@ const defaultProps = {
 }
 
 storiesOf('Search', module)
+  .addDecorator(TransparentDecorator)
   .add('empty', () => <Search {...defaultProps} query='' />)
   .add('query', () => <Search {...defaultProps} autoFocus />)
   .add('loading', () => <Search {...defaultProps} spotLoading />)
-  .add('fat empty', () => <Search {...defaultProps} fat query='' />)
-  .add('fat query', () => <Search {...defaultProps} fat autoFocus />)
-  .add('fat loading', () => <Search {...defaultProps} fat spotLoading />)
+
+storiesOf('Search/fat', module)
+  .addDecorator(GradientDecorator)
+  .add('empty', () => <Search {...defaultProps} fat query='' />)
+  .add('query', () => <Search {...defaultProps} fat autoFocus />)
+  .add('loading', () => <Search {...defaultProps} fat spotLoading />)
