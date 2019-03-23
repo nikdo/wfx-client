e6718f2f3bf40a9f714f7a2b824e049f8c7ef70c
Radek Matěj <radek@nikdo.cz>
2019-03-23 21:34:41 +0100

    Extract storybook whitespace decorator

--- src/search/components/Search.stories.jsx
+++ src/search/components/Search.stories.jsx
@@ -1,24 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import WhitespaceDecorator from '../../../.storybook/WhitespaceDecorator'
 import Search from './Search'
-
-const WhitespaceDecorator = (gradient = false) => storyFn => {
-  const style = {
-    padding: 24,
-    background: gradient
-      ? `linear-gradient(
-        120deg,
-        hsla(239, 35%, 51%, 0.83),
-        hsla(219, 44%, 50%, 0.83) 70%
-      )`
-      : 'transparent'
-  }
-  return <div style={style}>{storyFn()}</div>
-}
-
-const TransparentDecorator = WhitespaceDecorator()
-const GradientDecorator = WhitespaceDecorator(true)
 
 const defaultProps = {
   spots: [],
@@ -27,13 +11,13 @@ const defaultProps = {
 }
 
 storiesOf('Search', module)
-  .addDecorator(TransparentDecorator)
+  .addDecorator(WhitespaceDecorator())
   .add('empty', () => <Search {...defaultProps} query='' />)
   .add('query', () => <Search {...defaultProps} autoFocus />)
   .add('loading', () => <Search {...defaultProps} spotLoading />)
 
 storiesOf('Search/fat', module)
-  .addDecorator(GradientDecorator)
+  .addDecorator(WhitespaceDecorator({ inverted: true }))
   .add('empty', () => <Search {...defaultProps} fat query='' />)
   .add('query', () => <Search {...defaultProps} fat autoFocus />)
   .add('loading', () => <Search {...defaultProps} fat spotLoading />)
