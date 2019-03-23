59757ca1f381c04bea608fc551a691e5dce89a67
Radek Matěj <radek@nikdo.cz>
2019-03-23 18:06:20 +0100

    Add logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import SearchContainer from '../search/containers/Search'
 import styles from './Home.module.css'
+import { ReactComponent as Logo } from '../img/logo.svg'
 
 export default () => {
   return <header className={styles.header}>
-    <h1>Wind Freaks</h1>
+    <h1>
+      <Logo className={styles.logo} />
+      <span>Wind Freaks</span>
+    </h1>
     <SearchContainer className={styles.search} autoFocus fat />
   </header>
 }
