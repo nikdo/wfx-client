2a94dbb876ac88962e9122457932bbe21a253a6b
Radek Matěj <radek@nikdo.cz>
2019-03-24 23:23:36 +0100

    Add multiple pixel-density images

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './Attribution.module.css'
-import logo from '../img/dark-sky@2x.png'
+import logo1x from '../img/dark-sky@1x.png'
+import logo2x from '../img/dark-sky@2x.png'
+import logo3x from '../img/dark-sky@3x.png'
 
 export default ({ className }) => (
   <section className={classNames(styles.attribution, className)}>
     <a href='https://darksky.net/poweredby/'>
-      <img src={logo} alt='Powered by Dark Sky' />
+      <img
+        srcSet={`${logo1x}, ${logo2x} 2x, ${logo3x} 3x`}
+        src={logo3x}
+        alt='Powered by Dark Sky'
+      />
     </a>
   </section>
 )
