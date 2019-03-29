ad6f2f5ad6d80b7099889c95ed837b7b75b5eb6c
Radek Matěj <radek@nikdo.cz>
2019-03-29 17:58:40 +0100

    Open 3rd party forecasts in a new tab

--- src/components/OtherForecasts.jsx
+++ src/components/OtherForecasts.jsx
@@ -4,22 +4,27 @@ import { ReactComponent as WindguruLogo } from '../img/windguru.svg'
 import { ReactComponent as WindfinderLogo } from '../img/windfinder.svg'
 import styles from './OtherForecasts.module.css'
 
+const newTab = {
+  target: '_blank',
+  rel: 'noopener noreferrer'
+}
+
 export default () => (
   <section className='layout-section'>
     <p>Other forecasts:</p>
     <ul className={styles.links}>
       <li>
-        <a href='https://www.windy.com/'>
+        <a href='https://www.windy.com/' {...newTab}>
           <WindyLogo />
         </a>
       </li>
       <li>
-        <a href='https://www.windguru.cz/'>
+        <a href='https://www.windguru.cz/' {...newTab}>
           <WindguruLogo />
         </a>
       </li>
       <li>
-        <a href='https://www.windfinder.com/'>
+        <a href='https://www.windfinder.com/' {...newTab}>
           <WindfinderLogo />
         </a>
       </li>
