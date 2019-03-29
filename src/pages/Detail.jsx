3187529de5e6ea1b48c05207f1b04f0f69c64c45
Radek Matěj <radek@nikdo.cz>
2019-03-29 18:37:51 +0100

    Add detail bottom whitespace

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,10 +3,11 @@ import Header from '../components/Header'
 import SpotTitle from '../components/SpotTitle'
 import Chart from '../chart/components/Chart'
 import OtherForecasts from '../components/OtherForecasts'
+import styles from './Detail.module.css'
 
 export default ({ spotDetail }) => <>
   <Header />
-  <main>
+  <main className={styles.main}>
     <SpotTitle spot={spotDetail} />
     <Chart spotId={spotDetail._id} forecast={spotDetail.forecast} />
     <OtherForecasts
