2559abbace8c6f79d10cb7610a95963f74201dae
Radek Matěj <radek@nikdo.cz>
2019-03-30 13:03:05 +0100

    Merge header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Attribution from '../components/Attribution'
 import SearchContainer from '../search/containers/Search'
+import { ReactComponent as Logo } from '../img/logo.svg'
 import styles from './Header.module.css'
 
-export default () => (
-  <header className={styles.topHeader}>
-    <SearchContainer />
-  </header>
+export default ({ fullSize = false }) => (
+  fullSize
+    ? (
+      <header className={styles.header}>
+        <h1>
+          <Logo className={styles.logo} />
+          <span>Wind Freaks</span>
+        </h1>
+        <SearchContainer className={styles.search} autoFocus fat />
+        <Attribution className={styles.attribution} />
+      </header>
+    )
+    : (
+      <header className={styles.topHeader}>
+        <SearchContainer />
+      </header>
+    )
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
-import SearchContainer from '../search/containers/Search'
-import Attribution from '../components/Attribution'
-import styles from './Home.module.css'
-import { ReactComponent as Logo } from '../img/logo.svg'
+import Header from '../components/Header'
 
 export default () => {
-  return <header className={styles.header}>
-    <h1>
-      <Logo className={styles.logo} />
-      <span>Wind Freaks</span>
-    </h1>
-    <SearchContainer className={styles.search} autoFocus fat />
-    <Attribution className={styles.attribution} />
-  </header>
+  return <Header fullSize />
 }
