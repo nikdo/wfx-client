90f714d2684448a67c88b7f2bf6e60f8166535cb
Radek Matěj <radek@nikdo.cz>
2019-03-30 13:43:42 +0100

    Create shared header class name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import classNames from 'classnames'
 import Attribution from '../components/Attribution'
 import SearchContainer from '../search/containers/Search'
 import { ReactComponent as Logo } from '../img/logo.svg'
 import styles from './Header.module.css'
 
 export default ({ fullSize = false }) => (
-  <header className={fullSize ? styles.header : styles.topHeader}>
+  <header className={classNames(styles.header, fullSize ? styles.full : styles.top)}>
     {fullSize &&
       <h1>
         <Logo className={styles.logo} />
