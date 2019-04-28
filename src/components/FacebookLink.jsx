768953c8c0d6f78613b89ca8810d60acfb207cd8
Radek Matěj <radek@nikdo.cz>
2019-04-28 14:24:59 +0200

    Add Facebook icon

--- src/components/FacebookLink.jsx
+++ src/components/FacebookLink.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import newTabProps from './newTabProps'
+import { ReactComponent as FacebookLogo } from '../img/facebook.svg'
 import styles from './FacebookLink.module.css'
 
 const URL = 'https://www.facebook.com/windfreaksnet/'
 
 export default () => (
   <a className={styles.link} href={URL} {...newTabProps}>
-    Follow us<br />on Facebook
+    <FacebookLogo />
+    <span>
+      Follow us<br />
+      on Facebook
+    </span>
   </a>
 )
