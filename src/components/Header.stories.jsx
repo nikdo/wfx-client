8cc416e2df7b44617bcdfcfd12dc3d95d6520e17
Radek Matěj <radek@nikdo.cz>
2019-04-28 14:33:15 +0200

    Fix storybook router issues

--- src/components/Header.stories.jsx
+++ src/components/Header.stories.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import StoryRouter from 'storybook-react-router'
 import Header from './Header'
 import ProviderDecorator from '../../.storybook/ProviderDecorator'
 
 storiesOf('Header', module)
+  .addDecorator(StoryRouter())
   .addDecorator(ProviderDecorator)
   .add('top', () => <Header />)
   .add('fullSize', () => <Header fullSize />)
