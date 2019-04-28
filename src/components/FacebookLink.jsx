bd292c88992f1f8bf8dc11b710d2a47e5afa3ba3
Radek Matěj <radek@nikdo.cz>
2019-04-28 13:43:37 +0200

    Add Facebook link

---
+++ src/components/FacebookLink.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './FacebookLink.module.css'
+
+export default () => (
+  <a className={styles.link} href='https://www.facebook.com/windfreaksnet/'>
+    Follow us<br />on Facebook
+  </a>
+)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Attribution from './Attribution'
+import FacebookLink from './FacebookLink'
 import styles from './Footer.module.css'
 
 export default () => (
   <section className={styles.footer}>
     <Attribution />
+    <FacebookLink />
   </section>
 )
