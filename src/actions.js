7f9044f808db08be8f2c5d332b5989f315819ba6
Radek Matěj <radek@nikdo.cz>
2020-06-15 16:04:55 +0200

    Move daylight calculation to selector

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,4 @@
-import moment from 'moment-timezone'
 import history from './history'
-import roundHours from './roundHours'
 /* Source: https://github.com/umpirsky/country-list/blob/master/data/en_US/country.json */
 import countries from './countries.json'
 
@@ -13,18 +11,8 @@ const deserializeSpot = spot => ({
   ...countryCodeToCountry(spot),
   forecast: spot.forecast.map(frame => ({
     ...frame,
-    time: moment.unix(frame.time).tz(spot.timezone),
     windSpeed: Math.round(frame.windSpeed * 10) / 10,
-    windGust: Math.round(frame.windGust * 10) / 10,
-    isDaylight: spot.daylight
-      .map(day => ({
-        sunriseTime: roundHours(day.sunriseTime),
-        sunsetTime: roundHours(day.sunsetTime)
-      }))
-      .some(day =>
-        day.sunriseTime <= frame.time &&
-        frame.time <= day.sunsetTime
-      )
+    windGust: Math.round(frame.windGust * 10) / 10
   })),
   forecasts: spot.forecasts || {}
 })
--- src/selectors.js
+++ src/selectors.js
@@ -1,1 +1,25 @@
-export const getSpotDetail = state => state.spotDetail
+import moment from 'moment-timezone'
+import roundHours from './roundHours'
+
+const getSpotDaylightRounded = state => state.spotDetail &&
+  state.spotDetail.daylight
+    .map(day => ({
+      sunriseTime: roundHours(day.sunriseTime),
+      sunsetTime: roundHours(day.sunsetTime)
+    }))
+
+export const getSpotDetail = state => {
+  const daylight = getSpotDaylightRounded(state)
+  const spot = state.spotDetail
+  return spot && ({
+    ...spot,
+    forecast: spot.forecast.map(frame => ({
+      ...frame,
+      time: moment.unix(frame.time).tz(spot.timezone),
+      isDaylight: daylight.some(day =>
+        day.sunriseTime <= frame.time &&
+        frame.time <= day.sunsetTime
+      )
+    }))
+  })
+}
