b00f7c820f58b58e7ed83e310a5888d109c6446a
Radek Matěj <radek@nikdo.cz>
2020-06-15 14:56:10 +0200

    Round sunrise and sunset times

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 import moment from 'moment-timezone'
 import history from './history'
+import roundHours from './roundHours'
 /* Source: https://github.com/umpirsky/country-list/blob/master/data/en_US/country.json */
 import countries from './countries.json'
 
@@ -15,10 +16,15 @@ const deserializeSpot = spot => ({
     time: moment.unix(frame.time).tz(spot.timezone),
     windSpeed: Math.round(frame.windSpeed * 10) / 10,
     windGust: Math.round(frame.windGust * 10) / 10,
-    isDaylight: spot.daylight.some(day =>
-      day.sunriseTime <= frame.time &&
-      frame.time <= day.sunsetTime
-    )
+    isDaylight: spot.daylight
+      .map(day => ({
+        sunriseTime: roundHours(day.sunriseTime),
+        sunsetTime: roundHours(day.sunsetTime)
+      }))
+      .some(day =>
+        day.sunriseTime <= frame.time &&
+        frame.time <= day.sunsetTime
+      )
   })),
   forecasts: spot.forecasts || {}
 })
