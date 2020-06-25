3ec6657e8ecf4f9ffb1e8bb64d244a3d3503b269
Radek Matěj <radek@nikdo.cz>
2020-06-25 11:59:39 +0200

    Pass through props in search

--- src/search/containers/Search.jsx
+++ src/search/containers/Search.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { getSpots, getSearchQuery, getSpotLoading } from 'selectors'
 import { fetchSpotDetail } from 'actions'
 import { searchQueryChange } from '../reducer'
-import { getSpots, getSearchQuery, getSpotLoading } from 'selectors'
 import Search from '../components/Search'
 
-export default ({ autoFocus, fat, className }) => {
+export default props => {
   const spots = useSelector(getSpots)
   const searchQuery = useSelector(getSearchQuery)
   const spotLoading = useSelector(getSpotLoading)
   const dispatch = useDispatch()
   return (
     <Search
-      autoFocus={autoFocus}
-      fat={fat}
-      className={className}
-      onSpotSelected={id => fetchSpotDetail(dispatch, id)}
+      {...props}
       spots={spots}
       query={searchQuery}
       spotLoading={spotLoading}
       onChange={query => dispatch(searchQueryChange(query))}
+      onSpotSelected={id => fetchSpotDetail(dispatch, id)}
     />
   )
 }
