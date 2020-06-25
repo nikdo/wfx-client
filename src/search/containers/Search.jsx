059343b7423ab26ac39323cafed8978a6a6bd727
Radek Matěj <radek@nikdo.cz>
2020-06-25 11:57:28 +0200

    Use react-redux hooks in search

--- src/search/containers/Search.jsx
+++ src/search/containers/Search.jsx
@@ -1,32 +1,25 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { fetchSpotDetail } from 'actions'
 import { searchQueryChange } from '../reducer'
 import { getSpots, getSearchQuery, getSpotLoading } from 'selectors'
 import Search from '../components/Search'
 
-class SearchContainer extends Component {
-  render () {
-    const { autoFocus, fat, className, spots, searchQuery, spotLoading } = this.props
-    return (
-      <Search
-        autoFocus={autoFocus}
-        fat={fat}
-        className={className}
-        onSpotSelected={id => fetchSpotDetail(this.props.dispatch, id)}
-        spots={spots}
-        query={searchQuery}
-        spotLoading={spotLoading}
-        onChange={query => this.props.dispatch(searchQueryChange(query))}
-      />
-    )
-  }
+export default ({ autoFocus, fat, className }) => {
+  const spots = useSelector(getSpots)
+  const searchQuery = useSelector(getSearchQuery)
+  const spotLoading = useSelector(getSpotLoading)
+  const dispatch = useDispatch()
+  return (
+    <Search
+      autoFocus={autoFocus}
+      fat={fat}
+      className={className}
+      onSpotSelected={id => fetchSpotDetail(dispatch, id)}
+      spots={spots}
+      query={searchQuery}
+      spotLoading={spotLoading}
+      onChange={query => dispatch(searchQueryChange(query))}
+    />
+  )
 }
-
-const mapStateToProps = state => ({
-  spots: getSpots(state),
-  searchQuery: getSearchQuery(state),
-  spotLoading: getSpotLoading(state)
-})
-
-export default connect(mapStateToProps)(SearchContainer)
