14b37cda2f1ea800a16254b892845bf3f2428e20
Radek Matěj <radek@nikdo.cz>
2020-06-27 22:09:08 +0200

    Extract setting country name

--- src/selectors.js
+++ src/selectors.js
@@ -1,4 +1,5 @@
 import moment from 'moment-timezone'
+import { pipe } from 'functional'
 /* Source: https://github.com/umpirsky/country-list/blob/master/data/en_US/country.json */
 import countries from './countries.json'
 import roundHours from './roundHours'
@@ -25,10 +26,12 @@ export const daylightToDarkness = days => days
     ]
   }, [{}])
 
-export const getSpots = state => state.spots.map(spot => ({
+const setCountryName = spot => ({
   ...spot,
   country: countries[spot.country]
-}))
+})
+
+export const getSpots = state => state.spots.map(setCountryName)
 export const getSearchQuery = state => state.searchQuery
 export const getSpotLoading = state => state.spotLoading
 
@@ -36,23 +39,25 @@ export const getSpotDetail = state => {
   const daylightRounded = getSpotDaylightRounded(state)
   const darkness = daylightToDarkness(daylightRounded)
   const spot = state.spotDetail
-  return spot && ({
-    ...spot,
-    country: countries[spot.country],
-    weather: {
-      ...spot.weather,
-      hourly: spot.weather.hourly.map(frame => ({
-        ...frame,
-        time: moment.unix(frame.time).tz(spot.timezone),
-        isDaylight: daylightRounded.some(day =>
-          day.sunriseTime <= frame.time &&
-          frame.time <= day.sunsetTime
-        )
-      })),
-      darkness: darkness.map(night => ({
-        start: night.start && moment.unix(night.start).tz(spot.timezone),
-        end: night.end && moment.unix(night.end).tz(spot.timezone)
-      }))
-    }
-  })
+  return spot && pipe(
+    setCountryName,
+    spot => ({
+      ...spot,
+      weather: {
+        ...spot.weather,
+        hourly: spot.weather.hourly.map(frame => ({
+          ...frame,
+          time: moment.unix(frame.time).tz(spot.timezone),
+          isDaylight: daylightRounded.some(day =>
+            day.sunriseTime <= frame.time &&
+            frame.time <= day.sunsetTime
+          )
+        })),
+        darkness: darkness.map(night => ({
+          start: night.start && moment.unix(night.start).tz(spot.timezone),
+          end: night.end && moment.unix(night.end).tz(spot.timezone)
+        }))
+      }
+    })
+  )(spot)
 }
