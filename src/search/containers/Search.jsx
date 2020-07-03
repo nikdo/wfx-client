8adbfcdca928a90335d7cbfa82e70190615cdc4d
Radek Matěj <radek@nikdo.cz>
2020-07-03 16:26:34 +0200

    Use useHistory hook

--- src/search/containers/Search.jsx
+++ src/search/containers/Search.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { getSpots, getSearchQuery, getSpotLoading } from 'selectors'
 import { fetchSpotDetail } from 'actions'
 import { searchQueryChange } from '../reducer'
 import Search from '../components/Search'
-import history from '../../history'
 
 export default props => {
   const spots = useSelector(getSpots)
   const searchQuery = useSelector(getSearchQuery)
   const spotLoading = useSelector(getSpotLoading)
   const dispatch = useDispatch()
+  const history = useHistory()
 
   const handleSpotSelected = id =>
     fetchSpotDetail(dispatch, id)
