99929c0a5974f836161dd487be1bf6fa80b30a19
Radek Matěj <radek@nikdo.cz>
2020-07-04 16:19:20 +0200

    Use react-helmet to set title

--- src/components/SpotTitle.jsx
+++ src/components/SpotTitle.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import styles from './SpotTitle.module.css'
 
 export default ({ spot }) => (
   <header className='layout-section'>
+    <Helmet>
+      <title>{spot.name}</title>
+    </Helmet>
     <h1 className={styles.title}>{spot.name}</h1>
     <p className={styles.location}>
       {spot.region && spot.region + ', '}
