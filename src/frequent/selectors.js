9f022b0490a6876ded7487599d2a995af9cb5078
Radek Matěj <radek@nikdo.cz>
2020-08-22 22:13:15 +0200

    Show real visited spot IDs on homepage

--- src/frequent/selectors.js
+++ src/frequent/selectors.js
@@ -1,1 +1,2 @@
-export const getFrequentSpots = () => ['54f54e7f4509b82452437dc2', '5d15194a738ed92144e8e5d5', '561242e00ef5750a55ee0860']
+export const getFrequentSpots = state =>
+  Object.keys(state.visits)
