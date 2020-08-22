5913e1ff893106313da760ebc5bf08b75f77676b
Radek Matěj <radek@nikdo.cz>
2020-08-22 22:21:46 +0200

    Link to spot

--- src/frequent/FrequentSpotsContainer.jsx
+++ src/frequent/FrequentSpotsContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { getFrequentSpots } from './selectors'
 
 export default () => {
@@ -7,7 +8,11 @@ export default () => {
 
   return (
     <ul>
-      {spots.map(spot => <li key={spot}>{spot}</li>)}
+      {spots.map(id => (
+        <li key={id}>
+          <Link to={`/${id}`}>{id}</Link>
+        </li>
+      ))}
     </ul>
   )
 }
